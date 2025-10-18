4d94a2377f643bfd54f32f242f7d93a6fee71f91
thayna-gimenez <thaynagmnz@gmail.com>
2025-10-18 13:11:32 -0400

    Janela criada e pastas organizadas

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import*
+from tkinter import Tk, StringVar, ttk
+
+# Criando janela
+janela = Tk()
+janela.title('')
+janela.geometry('900x600')
+janela.configure(background="#e9edf5")
+janela.resizable(width=TRUE, height=TRUE)
+
+janela.mainloop()
